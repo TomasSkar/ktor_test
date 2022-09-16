2aea08f683fd5fa769dc746eae7f610182b76c00
TomasSkar <tomas.skarnulis@trafi.com>
2022-09-16 17:29:22 +0300

    more setup;

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,11 +3,11 @@ package com.example
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.server.application.*
 
-fun main() {
-    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
-        configureSerialization()
-        setupKoin()
-        configureRouting()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun Application.module() {
+    configureSerialization()
+    setupKoin()
+    configureRouting()
 }
